82b4dc1631bd4b219f2738cbe0be94e5640956ec
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-10-08 16:26:45 +0530

    add sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Link from "next/link";
-import { RxSketchLogo, RxDashboard, RxPerson } from "react-icons/rx";
+import { RxSketchLogo, RxPerson } from "react-icons/rx";
 import { HiOutlineShoppingBag } from "react-icons/hi";
-import { FiSettings } from "react-icons/fi";
+import { AiFillHome, AiOutlineMail } from "react-icons/ai";
 
 interface SidebarProps {
   children: React.ReactNode;
@@ -11,8 +11,8 @@ interface SidebarProps {
 const Sidebar: React.FC<SidebarProps> = ({ children }) => {
   return (
     <div className="flex">
-      <div className="fixed w-20 h-screen p-4 bg-white border-r-[1px] flex flex-col justify-between">
-        <div className="flex flex-col items-center">
+      <div className="fixed sm:w-20 md:w-40 lg:w-60 h-screen p-4 bg-white border-r-[1px] flex flex-col justify-between">
+        <div className="flex flex-col sm:items-center md:items-start">
           <Link href="/">
             <div className="bg-purple-800 text-white p-3 rounded-2xl inline-block">
               <RxSketchLogo size={20} />
@@ -20,24 +20,36 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
           </Link>
           <span className="border-b-[1px] border-gray-300 w-full p-2"></span>
           <Link href="/">
-            <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-2xl inline-block">
-              <RxDashboard size={20} />
-            </div>
-          </Link>
-          <Link href="/customer">
-            <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-2xl inline-block">
-              <RxPerson size={20} />
-            </div>
-          </Link>
-          <Link href="/orders">
-            <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-2xl inline-block">
-              <HiOutlineShoppingBag size={20} />
+            <div className="flex flex-row items-center">
+              <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 mx-1 p-3 rounded-2xl inline-block">
+                <AiFillHome size={20} />
+              </div>
+              Home
             </div>
           </Link>
           <Link href="/">
-            <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-2xl inline-block">
-              <FiSettings size={20} />
+            <div className="flex flex-row items-center">
+              <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 mx-1 p-3 rounded-2xl inline-block">
+                <RxPerson size={20} />
+              </div>
+              Know Us
             </div>
+          </Link>
+          <Link href="/">
+            <div className="flex flex-row items-center">
+              <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-2xl inline-block">
+                <HiOutlineShoppingBag size={20} />
+              </div>
+              Services
+            </div>
+          </Link>
+          <Link href="/">
+          <div className="flex flex-row items-center">
+            <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-2xl inline-block">
+              <AiOutlineMail size={20} />
+            </div>
+            Contact Us
+          </div>
           </Link>
         </div>
         <div></div>
